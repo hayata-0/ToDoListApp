497d2484537a6d321ed0ac0a9763a04490a7e029
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-16 11:24:00 +0900

    first commit

--- TodoListApp/ContentView.swift
+++ TodoListApp/ContentView.swift
@@ -1,21 +1,51 @@
-//
-//  ContentView.swift
-//  TodoListApp
-//
-//  Created by 大野颯太 on 2022/01/16.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var newItem:String = ""
+    @State var toDoList:[String] = ["課題", "掃除"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Text("新しい予定の追加").font(.largeTitle).padding(.leading)
+                Spacer()
+            }
+            
+            
+            HStack{
+                TextField("新しい予定を入力",text: $newItem).textFieldStyle(RoundedBorderTextFieldStyle()).frame(width: 300)
+            
+                Button(action:{
+                    self.toDoList.append(self.newItem)
+                    self.newItem = ""
+                }) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius:5).frame(width: 50, height: 30)
+                            .foregroundColor(.green)
+                    
+                        Text("追加").foregroundColor(.white)
+                    }
+                }
+            }
+            
+            HStack{
+                Text("To Do List").font(.largeTitle).padding(.leading)
+                Spacer()
+            }
+            
+            List(toDoList,id: \.self){ item in
+                Text(item)
+            }
+            
+            Spacer()
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
